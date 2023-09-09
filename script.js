974e6980b7f741e933d8a4f8a832483165673cb6
unknown <mikeweinstein183@gmail.com>
2023-09-09 13:57:09 -0400

    updated JS

--- script.js
+++ script.js
@@ -9,16 +9,20 @@ function updatePhoneModels() {
     // Toggle visibility of the input field for "Other"
     if (selectedDevice === 'Other') {
         otherDeviceInput.style.display = 'block';
+        modelSelect.disabled = true;  // Disable dropdown
     } else {
         otherDeviceInput.style.display = 'none';
+        modelSelect.disabled = false;  // Enable dropdown
     }
+    
 
     // Define options for each device
     const deviceOptions = {
-        'iPhone': ['--', 'iPhone 11', 'iPhone 12', 'iPhone 13'],
-        'Samsung': ['--', 'Galaxy S20', 'Galaxy S21', 'Galaxy S22'],
-        'Google Pixel': ['--', 'Pixel 4a', 'Pixel 5', 'Pixel 6']
-    };
+            'iPhone': ['--', 'iPhone 6s', 'iPhone 6s Plus', 'iPhone 7', 'iPhone 7 Plus', 'iPhone 8', 'iPhone 8 Plus', 'iPhone X', 'iPhone XR', 'iPhone XS', 'iPhone XS Max', 'iPhone 11', 'iPhone 11 Pro', 'iPhone 11 Pro Max', 'iPhone 12', 'iPhone 12 Mini', 'iPhone 12 Pro', 'iPhone 12 Pro Max', 'iPhone 13', 'iPhone 13 Mini', 'iPhone 13 Pro', 'iPhone 13 Pro Max'],
+            'Samsung': ['--', 'Galaxy S7', 'Galaxy S7 Edge', 'Galaxy S8', 'Galaxy S8+', 'Galaxy S9', 'Galaxy S9+', 'Galaxy S10', 'Galaxy S10e', 'Galaxy S10+', 'Galaxy S20', 'Galaxy S20+', 'Galaxy S20 Ultra', 'Galaxy S21', 'Galaxy S21+', 'Galaxy S21 Ultra'],
+            'Google Pixel': ['--', 'Pixel 3', 'Pixel 3 XL', 'Pixel 3a', 'Pixel 3a XL', 'Pixel 4', 'Pixel 4 XL', 'Pixel 4a', 'Pixel 5', 'Pixel 5a', 'Pixel 6', 'Pixel 6 Pro']
+        };
+        
 
     // Clear existing model options
     const modelSelect = document.getElementById('model');
@@ -36,21 +40,14 @@ function updatePhoneModels() {
 const deviceSelect = document.getElementById('deviceSelect');
 deviceSelect.addEventListener('change', updatePhoneModels);
 
-function handleSubmit(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const device = document.getElementById('deviceSelect').value;
-    const otherDevice = document.getElementById('otherDevice').value;
-    const responseMessage = document.getElementById('responseMessage');
-
-    responseMessage.textContent = `Thanks, ${name}! We'll reach out to you shortly. You've selected ${device}${device === 'Other' ? ': ' + otherDevice : ''}.`;
-    responseMessage.classList.add('visible');
-
-    document.getElementById('name').value = '';
-    document.getElementById('deviceSelect').value = '';
-    document.getElementById('otherDevice').value = '';
+if (selectedDevice === 'Other') {
+    otherDeviceInput.style.display = 'block';
+    modelSelect.disabled = true;  // Disable dropdown
+} else {
+    otherDeviceInput.style.display = 'none';
+    modelSelect.disabled = false;  // Enable dropdown
 }
+
 
 document.getElementById('responseMessage').addEventListener('click', () => {
     document.getElementById('responseMessage').classList.remove('visible');
